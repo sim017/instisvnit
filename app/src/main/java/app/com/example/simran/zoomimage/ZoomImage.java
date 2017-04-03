c4e497f48f48b6d52dc073dae37f8771db3a5628
Simran Bawkar <sim017bawkar@gmail.com>
2017-04-03 16:48:58 +0530

    drawer

--- app/src/main/java/app/com/example/simran/zoomimage/ZoomImage.java
+++ app/src/main/java/app/com/example/simran/zoomimage/ZoomImage.java
@@ -1,23 +1,164 @@
 package app.com.example.simran.zoomimage;
 
-import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class ZoomImage extends Activity {
+public class ZoomImage extends AppCompatActivity {
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mDrawerTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mTitle="hello";
+    private ArrayAdapter<String> mAdapter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_zoom_image);
 
         ZoomImageView imageView = new ZoomImageView(this, getWindow()
                 .getWindowManager().getDefaultDisplay().getOrientation());
         imageView.setImage(this.getResources().getDrawable(R.drawable.temp),
                 this);
-        ZoomImageView v=new ZoomImageView(this,getWindow().getWindowManager().getDefaultDisplay().getOrientation());
-        v.setImage(this.getResources().getDrawable(R.drawable.marker),this);
+       // ZoomImageView v=new ZoomImageView(this,getWindow().getWindowManager().getDefaultDisplay().getOrientation());
+       // v.setImage(this.getResources().getDrawable(R.drawable.marker),this);
 
+
+        mTitle = mDrawerTitle = getTitle();
+        mPlanetTitles = getResources().getStringArray(R.array.planet_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    //    mDrawerLayout.setScrimColor(Color.parseColor("#00FFFFF"));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);//invalidateOptionsMenu();
+            }
+
+            //    mDrawerList = (ListView)findViewById(R.id.left_drawer);
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Log.d("drawer", "onDrawerOpened: ");
+                getSupportActionBar().setTitle(mDrawerTitle);invalidateOptionsMenu();
+            }
+        };
+        mDrawerList=(ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+
+        // Set the drawer toggle as the DrawerListener
+
+        // selectItem(0);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeButtonEnabled(true);
         this.setContentView(imageView);
     }
+
+
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    private void selectItem(int position) {
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+
+    // @Override
+    // public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    //   int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    // if (id == R.id.action_settings) {
+    //   return true;
+    //}
+
+//        return super.onOptionsItemSelected(item);
+    //  }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
 /*public class ZoomImage extends AppCompatActivity {
 
     @Override
